83c44bc358654b964b4a706c3ba6020b5390f581
Marco Sandrin <m.sandrin21@imperial.ac.uk>
2025-09-03 21:55:04 +0100

    Improve portability by removing hardcoded paths
    Make should now find mcpp as long as it is in the same directory as
    cronos. Also fix trailing whitespace.
    Required environment variable: SUNDIALS_HOME

--- src/makeoptions.mk
+++ src/makeoptions.mk
@@ -1,11 +1,13 @@
 # THIRD-PARTY LIBRARIES <<-- CHANGE AS APPROPRIATE -->>
 
-PATH_MC = $(shell cd $(HOME)/Programs/bitbucket/mcpp40 ; pwd)
-include $(PATH_MC)/src/makeoptions.mk
+PATH_CRONOS_MK := $(dir $(lastword $(MAKEFILE_LIST)))
 
-PATH_CRONOS    = $(shell cd $(HOME)/Programs/bitbucket/cronos40; pwd)
+include $(abspath $(PATH_CRONOS_MK)../../mcpp/src/makeoptions.mk)
+PATH_MC = $(abspath $(PATH_CRONOS_MK)../../mcpp)
 
-PATH_SUNDIALS = /opt/sundials-7.4.0
+PATH_CRONOS = $(abspath ../)
+
+PATH_SUNDIALS = $(SUNDIALS_HOME)
 LIB_SUNDIALS = -L$(PATH_SUNDIALS)/lib -lsundials_sunlinsolklu -lsundials_cvodes -lsundials_nvecserial -lsundials_core -llapack -lblas
 INC_SUNDIALS = -I/usr/include/suitesparse -I$(PATH_SUNDIALS)/include
 FLAG_SUNDIALS = -DCRONOS__WITH_KLU
@@ -17,8 +19,8 @@ OPTIM = -O2
 DEBUG = #-g
 WARN  = -Wall -Wno-misleading-indentation -Wno-unknown-pragmas -Wno-parentheses -Wno-unused-result
 CPP17 = -std=c++17
-CC    = gcc-13
-CPP   = g++-13
+CC    = gcc
+CPP   = g++
 # CPP   = icpc
 
 # <<-- NO CHANGE BEYOND THIS POINT -->>
@@ -27,7 +29,7 @@ FLAG_CPP  = $(DEBUG) $(OPTIM) $(CPP17) $(WARN) $(PROF)
 LINK      = $(CPP)
 FLAG_LINK = $(PROF)
 
-FLAG_CRONOS = -fPIC $(FLAG_MC) $(FLAG_SUNDIALS) 
+FLAG_CRONOS = -fPIC $(FLAG_MC) $(FLAG_SUNDIALS)
 LIB_CRONOS  = $(LIB_MC) $(LIB_SUNDIALS)
 INC_CRONOS  = -I$(PATH_CRONOS)/src $(INC_MC) $(INC_SUNDIALS)
 
